e79f70e587a76f9180bf607d980aa200a68e56ce
Nicolas B. <nicolasboulenc@gmail.com>
2025-03-06 14:15:04 +0000

    yet another commit

---
+++ ngctl.cpp
@@ -0,0 +1,29 @@
+
+#include <iostream>
+#include <cstring>
+// #include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    std::cout << argv[1] << "test\n";
+
+    if(argc == 0) {
+
+    }
+    else if(argc == 0) {
+
+    }
+    else if(argc == 0) {
+
+    }
+
+
+    if( strcmp(argv[0], "start") == 0 ) {
+        printf("Start.\n");
+    }   
+    else {
+        printf("Hello! This is a test prgoram.\n");
+    }
+
+    return 0;
+}
